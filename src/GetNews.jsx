0eb0357493c3c78ef1f859d5a0d5d6eea7d61844
Brandon Tate <tatesworkacc@gmail.com>
2024-11-15 14:29:20 -0600

    added fetch and input, button, and setState vals

--- src/GetNews.jsx
+++ src/GetNews.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react"
 
 function GetNews(){
+    const apiKey = 'e4c0f36fe7f446fa97faf58ddbd7318f'
+    const api =  `https://newsapi.org/v2/${query}?country=us&apiKey=${apiKey}`
+    const [query, setQuery] = useState('')
+
+    fetch()
 
     return(
 
