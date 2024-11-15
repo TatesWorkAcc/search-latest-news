c238556e50fa91d31ae85b0f613a4aeb1fec15a5
Brandon Tate <tatesworkacc@gmail.com>
2024-11-15 15:00:41 -0600

    made lists for array of data

--- src/GetNews.jsx
+++ src/GetNews.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react"
 
 function GetNews(){
-    const apiKey = 'e4c0f36fe7f446fa97faf58ddbd7318f'
-    const api =  `https://newsapi.org/v2/${endpoint}?q=${query}&apiKey=${apiKey}`
+ 
+  
     const [query, setQuery] = useState('bitcoin')      //what the users search
     const [endPoint, setEndPoint] = useState('everything')        // Top headlines /v2/top-headlines   Everything /v2/everything   Sources /v2/top-headlines/sources
+    const apiKey = 'e4c0f36fe7f446fa97faf58ddbd7318f'
+    const api =  `https://newsapi.org/v2/${endPoint}?q=${query}&apiKey=${apiKey}`
+
+    const [data, setData] = useState([])    // to hold arrays of data.
+
+    
 
     useEffect(() => {
         fetch(api)
@@ -16,8 +22,9 @@ function GetNews(){
                 console.log('Failed')
             }
         })
-        .then((data) => {
-            console.log(data)
+        .then((dataArray) => {
+            setData(dataArray.articles)
+            console.log(dataArray)
         })
         .catch((error) => {
             console.error('Error')
@@ -32,8 +39,13 @@ function GetNews(){
     return(
         <div>
             <input value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Search a topic"></input>
-            
             <button onClick={handleSearch} >Search</button>
+
+            <ul>
+                {data.map((item, index) => (
+                    <li key={index}>{item.author}</li>
+                ))}
+            </ul>
         </div>
     )
 
